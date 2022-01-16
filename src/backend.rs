d850eaa57a78e0f7b2fc5458d173721294e3da25
trevyn <trevyn-git@protonmail.com>
2022-01-16 19:34:44 +0300

    add stream sketch

--- src/backend.rs
+++ src/backend.rs
@@ -1,6 +1,5 @@
 use turbocharger::backend;
-use turbosql::Turbosql;
-use wasm_bindgen::prelude::*;
+use turbosql::{select, Turbosql};
 
 #[backend]
 #[derive(Turbosql, Default)]
@@ -16,5 +15,35 @@ async fn insert_person(p: Person) -> Result<i64, turbosql::Error> {
 
 #[backend]
 async fn get_person(rowid: i64) -> Result<Person, turbosql::Error> {
- turbosql::select!(Person "WHERE rowid = ?", rowid)
+ select!(Person "WHERE rowid = ?", rowid)
 }
+
+// N.B. Streams are not yet supported, but this is what the interface might look like:
+
+// #[backend]
+// fn stream_example() -> impl Stream<Item = u32> {
+//  async_stream::stream! {
+//   let mut i = 0;
+//   loop {
+//    yield i;
+//    i += 1;
+//    tokio::time::sleep(tokio::time::Duration::from_millis(2000)).await;
+//   }
+//  }
+// }
+
+// use async_stream::stream;
+
+// use futures_core::stream::Stream;
+// use futures_util::pin_mut;
+// use futures_util::stream::StreamExt;
+
+// #[tokio::main]
+// async fn main() {
+//  let s = zero_to_three();
+//  pin_mut!(s); // needed for iteration
+
+//  while let Some(value) = s.next().await {
+//   println!("got {}", value);
+//  }
+// }
