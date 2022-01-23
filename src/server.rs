4eb8207eabcebc45666ec9e09808179d014f7729
trevyn <trevyn-git@protonmail.com>
2022-01-23 16:14:14 +0300

    update for axum turbocharger

--- src/server.rs
+++ src/server.rs
@@ -18,6 +18,10 @@ async fn main() {
  #[folder = "build"]
  struct Frontend;
 
+ if std::env::var_os("RUST_LOG").is_none() {
+  std::env::set_var("RUST_LOG", "info")
+ }
+
  pretty_env_logger::init_timed();
  let opts = Opts::parse();
 
@@ -29,17 +33,18 @@ async fn main() {
  match (opts.key_path, opts.cert_path) {
   (Some(key_path), Some(cert_path)) => {
    eprintln!("Serving HTTPS on port {}", opts.port);
-   warp::serve(turbocharger::warp_routes(Frontend))
-    .tls()
-    .cert_path(cert_path)
-    .key_path(key_path)
-    .run(([0, 0, 0, 0], opts.port))
-    .await;
+   // warp::serve(turbocharger::warp_routes(Frontend))
+   //  .tls()
+   //  .cert_path(cert_path)
+   //  .key_path(key_path)
+   //  .run(([0, 0, 0, 0], opts.port))
+   //  .await;
   }
   (None, None) => {
    eprintln!("Serving (unsecured) HTTP on port {}", opts.port);
    opener::open(format!("http://127.0.0.1:{}", opts.port)).ok();
-   warp::serve(turbocharger::warp_routes(Frontend)).run(([0, 0, 0, 0], opts.port)).await;
+   let addr = std::net::SocketAddr::from(([0, 0, 0, 0], opts.port));
+   turbocharger::axum_server::serve::<Frontend>(&addr).await;
   }
   _ => eprintln!("Both key-path and cert-path must be specified for HTTPS."),
  }
