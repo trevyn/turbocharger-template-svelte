0edc2739cccacbb8eb99c78bbc88f837877abaee
trevyn <trevyn-git@protonmail.com>
2022-02-04 14:41:50 +0300

    test stream of results

--- src-frontend/App.svelte
+++ src-frontend/App.svelte
@@ -10,12 +10,19 @@
   console.log("Inserted rowid ", rowid);
  })();
 
- let stream_example_store = backend.stream_example();
+ let stream_example_result = backend.stream_example_result();
+ let stream_example = backend.stream_example();
 </script>
 
 Hello!<br />
 
-stream_example_store -> {$stream_example_store}
+stream_example -> {$stream_example}<br />
+
+stream_example_result -> {#await $stream_example_result then result}
+ {result}
+{:catch error}
+ Error: {error}
+{/await}<br />
 
 {#await backend.get_person(1n) then person}
  Name: {person.name}
--- src/backend.rs
+++ src/backend.rs
@@ -20,14 +20,41 @@ async fn get_person(rowid: i64) -> Result<Person, turbosql::Error> {
 
 // N.B. Streams are experimental!
 
+// #[backend]
+// async fn stream_example() -> yields!(u32) {
+//  let mut i = 0;
+//  loop {
+//   yield i;
+//   i += 1;
+//   tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+//  }
+// }
+
 #[backend]
-fn stream_example() -> impl Stream<Item = u32> {
+fn stream_example() -> impl Stream<Item = String> {
  turbocharger::async_stream::stream! {
   let mut i = 0;
   loop {
-   yield i;
+   yield format!("e{}", i*10);
    i += 1;
-   tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+   tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+  }
+ }
+}
+
+#[backend]
+#[tracked::tracked]
+fn stream_example_result() -> impl Stream<Item = Result<String, tracked::Error>> {
+ turbocharger::async_stream::try_stream! {
+  let mut i = 0;
+  loop {
+   yield format!("r{}", i);
+   i += 1;
+   if i == 5 {
+    u8::try_from(999u16)?;
+    // None?;
+   }
+   tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
   }
  }
 }
