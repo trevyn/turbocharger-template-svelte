1434ab2746e7b33f04282c3f3d823539587959a6
trevyn <230691+trevyn@users.noreply.github.com>
2022-07-31 18:47:51 -0400

    increase test timeout

--- src-frontend/tests/turbocharger.test.js
+++ src-frontend/tests/turbocharger.test.js
@@ -1,6 +1,7 @@
 import turbocharger_init, * as backend from "../turbocharger_generated";
 
 it("does stuff", async () => {
+ this.timeout(10000);
  await turbocharger_init();
  backend.set_socket_url("ws://localhost:8080/turbocharger_socket");
  let person = Object.assign(new backend.Person(), { name: "Bob" });
